3b66a068d4340bcd5eed5cd961a0fc56bdd103f3
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-02-21 09:53:34 +0530

    Assignment Solution of Trees

--- Assignment 5 Tree/1. Tree With All Traversal.cpp
+++ Assignment 5 Tree/1. Tree With All Traversal.cpp
@@ -4,47 +4,93 @@ using namespace std;
 
 struct TreeNode
 {
-        int data ;
-        TreeNode* right ;
-        TreeNode* left ;
+        int data;
+        TreeNode *right;
+        TreeNode *left;
 
         TreeNode(int data)
         {
-                this->data = data ;
-                right = NULL ;
-                left = NULL ;
+                this->data = data;
+                right = NULL;
+                left = NULL;
         }
 };
 
-void inOrderTraversal(TreeNode root)
+void inOrderTraversal(TreeNode *root)
 {
-        TreeNode temp = root ;
-       
+        TreeNode *temp = root;
+        if (temp == NULL)
+                return;
+
+        inOrderTraversal(temp->left);
+
+        cout << temp->data << " ";
+
+        inOrderTraversal(temp->right);
 }
 
-void preOrderTraversal(TreeNode root)
+void preOrderTraversal(TreeNode *root)
 {
-        TreeNode temp = root ;
-       
+        TreeNode *temp = root;
+
+        if (temp == NULL)
+                return;
+
+        cout << temp->data << " ";
+
+        preOrderTraversal(temp->left);
+
+        preOrderTraversal(temp->right);
 }
 
-void postOrderTraversal(TreeNode root)
+void postOrderTraversal(TreeNode *root)
 {
-        TreeNode temp = root ;
-       
+        TreeNode *temp = root;
+        if (temp == NULL)
+                return;
+
+        postOrderTraversal(temp->left);
+
+        postOrderTraversal(temp->right);
+
+        cout << temp->data << " ";
 }
 
-void levelOrderTraversal(TreeNode root)
+void levelOrderTraversal(TreeNode *root)
 {
-        TreeNode temp = root ;
-       
-}
+        TreeNode *temp = root;
+        if (root == NULL)
+                return;
 
+        
+        queue<TreeNode *> q;
+
+        q.push(root);
+
+        while (q.empty() == false)
+        {
+
+                TreeNode *temp = q.front();
+                cout << temp->data << " ";
+                q.pop();
+
+                if (temp->left != NULL)
+                        q.push(temp->left);
+
+
+                if (temp->right != NULL)
+                        q.push(temp->right);
+        }
+}
 
 int32_t main()
 {
-        TreeNode* root = new TreeNode(10);
+        TreeNode *root = new TreeNode(10);
         root->left = new TreeNode(5);
         root->right = new TreeNode(20);
-        return 0 ;
+        inOrderTraversal(root);
+        preOrderTraversal(root);
+        postOrderTraversal(root);
+        levelOrderTraversal(root);
+        return 0;
 }
